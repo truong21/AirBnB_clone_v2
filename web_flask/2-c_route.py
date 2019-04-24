4a4a376e857408799d3799d6f46976d2b85c5d99
truong21 <543@holbertonschool.com>
2019-04-24 20:17:02 +0000

    Flask web application that takes a variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Flask application to display "C" followed by the value of the text variable
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ prints Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """ prints HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_C(text):
+    """ prints "C" followed by value of the text var """
+    return "C %s" % text
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
