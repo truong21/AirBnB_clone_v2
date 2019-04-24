d4cddee92e35e3b662989ba75d1260d1882de792
truong21 <543@holbertonschool.com>
2019-04-24 07:37:58 +0000

    pep8 fixes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,10 +14,12 @@ def hello():
     """ prints Hello HBNB! """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_hbnb():
     """ prints HBNB """
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
