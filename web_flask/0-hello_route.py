55a500497e0bf0b7dd9a2fc56e649c27fcf0fed3
truong21 <543@holbertonschool.com>
2019-04-24 01:36:59 +0000

    starts a Flask Web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Flask application to print Hello HBNB
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ prints Hello HBNB! """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
